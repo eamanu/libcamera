4c5de4719c4fbfcc864c248255bf0761217331db
Emmanuel Arias <emmanuelarias30@gmail.com>
2019-06-02 23:49:53 +0000

    Delete debian files unnecessary up-to-date

--- debian/libcamera.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the libcamera package
-#
-0 4	* * *	root	[ -x /usr/bin/libcamera_maintenance ] && /usr/bin/libcamera_maintenance
--- debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-liblibcamera 0~190601 libcamera (>> 0~190601-0), libcamera (<< 0~190601-99)
