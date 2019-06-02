6707b60312e5c36b135b2c4a4ac0a27683a2bcdc
Emmanuel Arias <emmanuelarias30@gmail.com>
2019-06-02 14:42:44 +0000

    Adding debian files

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(libcamera):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="libcamera" command="/usr/bin/libcamera"
---
+++ debian/shlibs.local.ex
@@ -0,0 +1,1 @@
+liblibcamera 0~190601 libcamera (>> 0~190601-0), libcamera (<< 0~190601-99)
